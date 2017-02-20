886690df1347a5a480fb44d35c2cad3b349a7baa
Jonny Holupchinski <jonholup@gmail.com>
2017-02-20 14:17:09 -0600

    everything works!

--- client.js
+++ client.js
@@ -3,48 +3,31 @@ var yellowCount = 0;
 var greenCount = 0;
 var blueCount = 0;
 
-$(document).ready(function(){
+$(document).ready(function() {
   console.log('all ready');
-$('.color-button').on('click', function(){
-  var $thisColor = $(this).data("color");
-  colorCounter($thisColor);
+  $('.color-button').on('click', function() {
+    var $thisColor = $(this).data("color");
+    colorCounter($thisColor);
 
-});
+  });
 
+  function colorCounter(color) {
+    $('.container').append('<div class="color-cube ' + color + '"></div>');
+    if (color == 'red') {
+      redCount++;
+      $('#red').text(redCount);
+    } else if (color == 'yellow') {
+      yellowCount++;
+      $('#yellow').text(yellowCount);
+    } else if (color == 'green') {
+      greenCount++;
+      $('#green').text(greenCount);
+    } else if (color == 'blue') {
+      blueCount++;
+      $('#blue').text(blueCount);
+    }
 
-
-
-
-function colorCounter (color) {
-  $('.container').append('<div class="color-cube ' + color + '"></div>');
-  if (color == 'red'){
-    redCount++;
-    $('#red').append(redCount);
-  }else if (color == 'yellow') {
-    yellowCount++;
-    $('#yellow').append(yellowCount);
-  }else if (color == 'green') {
-    greenCount++;
-    $('#green').append(greenCount);
-  }else if (color == 'blue'){
-    blueCount++;
-    $('#blue').append(blueCount);
   }
 
-}
-
 
 });
-// if (this.data == 'red'){
-//   redCount++;
-//   $('#red').append(redCount);
-// } else if (this.data == 'yellow') {
-//   yellowCount++;
-//   $('#yellow').append(yellowCount);
-// } if else (this.data = 'green') {
-//   greenCount++;
-//   $('#green').append(greenCount);
-// } else {
-//   blueCount++;
-//   $('#blue').append(blueCount);
-// }
