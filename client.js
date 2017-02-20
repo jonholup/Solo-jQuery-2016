7f296ac15c341961c756851164149bf9e32ade91
Jonny Holupchinski <jonholup@gmail.com>
2017-02-20 14:13:25 -0600

    just need to parseInt and append

--- client.js
+++ client.js
@@ -1,16 +1,50 @@
+var redCount = 0;
+var yellowCount = 0;
+var greenCount = 0;
+var blueCount = 0;
+
 $(document).ready(function(){
   console.log('all ready');
 $('.color-button').on('click', function(){
   var $thisColor = $(this).data("color");
-  $('.container').append('<div class="color-cube ' + $thisColor + '"></div>');
+  colorCounter($thisColor);
+
+});
+
+
+
+
+
+function colorCounter (color) {
+  $('.container').append('<div class="color-cube ' + color + '"></div>');
+  if (color == 'red'){
+    redCount++;
+    $('#red').append(redCount);
+  }else if (color == 'yellow') {
+    yellowCount++;
+    $('#yellow').append(yellowCount);
+  }else if (color == 'green') {
+    greenCount++;
+    $('#green').append(greenCount);
+  }else if (color == 'blue'){
+    blueCount++;
+    $('#blue').append(blueCount);
+  }
+
+}
 
 
 });
-
-
-
-
-
-
-
-});
+// if (this.data == 'red'){
+//   redCount++;
+//   $('#red').append(redCount);
+// } else if (this.data == 'yellow') {
+//   yellowCount++;
+//   $('#yellow').append(yellowCount);
+// } if else (this.data = 'green') {
+//   greenCount++;
+//   $('#green').append(greenCount);
+// } else {
+//   blueCount++;
+//   $('#blue').append(blueCount);
+// }
